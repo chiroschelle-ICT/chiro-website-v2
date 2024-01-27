d5d750fc8991cc5d3f72d5f9a7536966658a86d4
Killian-Stella <killian.serluppens@gmail.com>
2024-01-27 22:35:17 +0100

    syntax change route files

--- API/routes/blogRoutes.js
+++ API/routes/blogRoutes.js
@@ -3,18 +3,28 @@ const router = express.Router();
 const blogPostsController = require('../controllers/blogController');
 
 // GET | ALl blogPosts
-router.get('/blogPosts', blogPostsController.getAllBlogPosts);
+router.get('/blogPosts', function(res) { 
+    blogPostsController.getAllBlogPosts
+});
 
 // GET | blogPost by Id
-router.get('/blogPosts/:id', blogPostsController.getBlogPostById);
+router.get('/blogPosts/:id', function(res) { 
+    blogPostsController.getBlogPostById
+});
 
 // POST | Add new blogpost
-router.post('/blogPost/addBlogpost', blogPostsController.postBlogPost);
+router.post('/blogPost/addBlogpost', function(res) { 
+    blogPostsController.postBlogPost
+});
 
 // PUT | Update blogpost
-router.put('/blogPost/editBlogPost/:id', blogPostsController.putBlogPost);
+router.put('/blogPost/editBlogPost/:id', function(res) { 
+    blogPostsController.putBlogPost
+});
 
 // DELETE | Delete blogpost by id
-router.delete('/blogPost/deletePost/:id', blogPostsController.deleteBlogPost);
+router.delete('/blogPost/deletePost/:id', function(res) { 
+    blogPostsController.deleteBlogPost
+});
 
 module.exports = router;
