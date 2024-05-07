669d4c7063d3501ab0143d5c9bb356e63b0df5a5
Killian-Stella <killian.serluppens@gmail.com>
2024-05-08 00:11:44 +0200

    added category to API

---
+++ chiro-website/src/app/Model/Category.ts
@@ -0,0 +1,14 @@
+export class Category {
+    id: null;
+    name: string;
+    color: string;
+    constructor(
+        i:null,
+        n:string,
+        c:string
+    ) {
+        this.id = i;
+        this.name = n;
+        this.color = c;
+    }
+}
