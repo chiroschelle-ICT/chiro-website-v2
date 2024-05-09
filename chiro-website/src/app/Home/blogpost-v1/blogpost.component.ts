c67bff0132fd8857b2de8917dfbf1d0b5d1ab08f
Killian-Stella <killian.serluppens@gmail.com>
2024-05-09 20:27:35 +0200

    tried adding the hour posted

--- chiro-website/src/app/Home/blogpost-v1/blogpost.component.ts
+++ chiro-website/src/app/Home/blogpost-v1/blogpost.component.ts
@@ -26,13 +26,20 @@ export class BlogpostComponent implements OnInit{
   categories: Category[] = [];
   infos: Info[] = [];
   users: Users[] = [];
-  cat!: Category;
+
+  timeDifference!: any;  
+  currentDate = new Date();
+
+  dateShow!: string
 
   constructor(private homeService : HomeService) {}
 
   ngOnInit() {
     this.homeService.getBlogposts().subscribe((data: Blogposts[]) => {
       this.blogposts = data;
+      for(let i = 0; i<this.blogposts.length; i++) {
+        this.calculateTimeDifference(i);
+      }
     });
     this.homeService.getCategory().subscribe((data : Category[]) => {
       this.categories = data;
@@ -44,6 +51,35 @@ export class BlogpostComponent implements OnInit{
       this.users = data;
     })
   }
+
+  calculateTimeDifference(id:number) {
+    // Assuming dateTimeDb and dateTimeNow are already defined
+    const dateTimeDb = new Date(this.blogposts[id].timePosted);
+    const dateTimeNow = new Date(); // Get current date and time
+
+    // Calculate the difference in milliseconds
+    const timeDifferenceMs = dateTimeNow.getTime() - dateTimeDb.getTime();
+
+    // Convert milliseconds to days, hours, and minutes
+    const daysDifference = Math.floor(timeDifferenceMs / (1000 * 60 * 60 * 24));
+    const remainingHours = Math.floor((timeDifferenceMs % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const remainingMinutes = Math.floor((timeDifferenceMs % (1000 * 60 * 60)) / (1000 * 60));
+
+    // Format the output
+    const formattedDifference = `${daysDifference} : ${remainingHours} : ${remainingMinutes}`;
+
+    console.log(formattedDifference); // Output: DD : HH : MM
+
+    if(daysDifference <= 0 && remainingHours <= 0) {
+      this.dateShow = "Net Gepost! "
+      console.log(`${daysDifference}`)
+    } else if (daysDifference <= 0) {
+      this.dateShow = remainingHours + "Uur Geleden"
+    }
+
+  }
+
+
 
 
 }
