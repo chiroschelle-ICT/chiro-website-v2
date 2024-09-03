7de9a2f2984536be1b9d0207c16ac68691724d9e
Killian-Stella <killian.serluppens@gmail.com>
2024-09-04 01:51:09 +0200

    gitignore

--- chiro-website/environment/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    production: true,
-    ENCRYPTION_KEY: 'c5758328be76ef8b31e0e204c6f5b997'
-  };
--- chiro-website/environment/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    production: false,
-    ENCRYPTION_KEY: 'c5758328be76ef8b31e0e204c6f5b997'
-  };
