3f48a5595a0ce7546d3de137f7f2fec50558de6b
Killian-Stella <killian.serluppens@gmail.com>
2024-10-02 19:00:15 +0200

    added goepie-list component

---
+++ chiro-website/src/app/Admin/Overview/gp-list/gp-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gp-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './gp-list.component.html',
+  styleUrl: './gp-list.component.css'
+})
+export class GpListComponent {
+
+}
