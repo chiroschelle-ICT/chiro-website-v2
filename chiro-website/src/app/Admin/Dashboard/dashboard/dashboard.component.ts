75ce8c5ff08ca473677115666095957d9961133a
Killian-Stella <killian.serluppens@gmail.com>
2024-10-08 23:42:18 +0200

    [FEATURE] Added base layout

--- chiro-website/src/app/Admin/Dashboard/dashboard/dashboard.component.ts
+++ chiro-website/src/app/Admin/Dashboard/dashboard/dashboard.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { GoepiePanelComponent } from '../goepie-panel/goepie-panel.component';
+import { BlogpostPanelComponent } from '../blogpost-panel/blogpost-panel.component';
+import { AccountPanelComponent } from '../account-panel/account-panel.component';
+import { ProgrammaPanelComponent } from '../programma-panel/programma-panel.component';
+import { GoepiePageComponent } from "../../../Goepie/goepie-page/goepie-page.component";
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
   imports: [
-    GoepiePanelComponent
-  ],
+    GoepiePanelComponent,
+    BlogpostPanelComponent,
+    AccountPanelComponent,
+    ProgrammaPanelComponent,
+    GoepiePageComponent
+],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
