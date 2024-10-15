6861d6d25c67aff26034726648b9922ccc2ab216
Killian-Stella <killian.serluppens@gmail.com>
2024-10-15 23:01:15 +0200

    Changes

--- chiro-website/src/app/Authentication/signup/signup.component.ts
+++ chiro-website/src/app/Authentication/signup/signup.component.ts
@@ -3,15 +3,21 @@ import { AuthService } from '../auth.service';
 import { AdminService } from '../../Admin/admin.service';
 import { LocalstorageService } from '../../Services/localstorage.service';
 import { Users } from '../../Model/Users';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+  ],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
 export class SignupComponent implements OnInit{
+
+  signupForm = new FormControl({
+  });
 
   sideBarColor!: string;
   loggedUN!: string;
